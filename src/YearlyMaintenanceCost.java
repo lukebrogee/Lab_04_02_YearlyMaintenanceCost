51259f38b753eb2df78d8f240ed9c224540ceb31
Luke Brogee <lukebrogee@gmail.com>
2022-09-17 18:05:32 -0400

    Added YearlyMaintenanceCost.java

---
+++ src/YearlyMaintenanceCost.java
@@ -0,0 +1,14 @@
+public class YearlyMaintenanceCost {
+    public static void main(String[] args) {
+        double springCost = 100.00;
+        double summerCost = 75.55;
+        double fallCost = 105.43;
+        double winterCost = 99.99;
+        double totalYearlyMaintenanceCost;
+
+
+        totalYearlyMaintenanceCost = springCost + summerCost + winterCost + fallCost;
+
+        System.out.println("The total yearly maintenance cost is " + totalYearlyMaintenanceCost);
+    }
+}
